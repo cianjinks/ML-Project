c822eb819dfabb89e3704e55c7aecdb1a4c6de94
cppavel <petrukhp@tcd.ie>
2022-11-23 15:27:21 +0000

    Finalize fetching extra data

--- data/fetch_extra_data.py
+++ data/fetch_extra_data.py
@@ -57,11 +57,46 @@ def get_player_stats(encrypted_summoner_id):
     
     return stats
 
+
+def read_file(filename: str):
+    f = open(filename, "r")
+    return json.load(f)
+
+def parse_player_data(filename):
+    player_data = read_file(filename)
+
+    extra_player_data = {}
+
+    count = 0
+
+    for puuid, data in player_data.items():
+        encrypted_summoner_id = data["encryptedSummonerId"]
+        champion_ids = data["championsPlayed"]
+
+        player_stats = get_player_stats(encrypted_summoner_id)
+
+        champion_stats = {}
+
+        for champion_id in champion_ids:
+            champion_stats[champion_id] = fetch_mastery(encrypted_summoner_id, champion_id)
+
+        extra_player_data[puuid] = {
+            "stats": player_stats,
+            "championMastery": champion_stats
+        }
+        count+=1
+
+        print(f"Processed: {count}/{len(player_data)} players")
+
+    with open("extra_player_data.json", "w+") as outfile:
+        json.dump(extra_player_data, outfile)
+        
+
 def test():
     champion_id = 34
     encrypted_summoner_id = "XT6zAd8EriCUWwvsTr6v27-XiHk4djIX6YTVaxMqyAEvN7Y"
     print(get_player_stats(encrypted_summoner_id))
     print(fetch_mastery(encrypted_summoner_id, champion_id))
-    
+
 if __name__ == "__main__":
-    test()
+    parse_player_data("player_data_example.json")
