fac944de969cd49d3d13f135335c780af2a80740
Cian Jinks <cjinks99@gmail.com>
2022-11-25 17:25:50 +0000

    Working on baseline predictor, flattened features in array

---
+++ model/baseline_model.py
@@ -0,0 +1,117 @@
+# This script creates a baseline predictor model for the 100 dataset.
+# The baseline predictor counts the total tier of players in the game and considers the team with highest combined tier to win
+
+import json
+from sklearn.model_selection import train_test_split
+
+MATCH_DATA_FILE = "small_dataset/match_data_100.json"
+PLAYER_DATA_FILE = "small_dataset/player_data_100.json"
+EXTRA_PLAYER_DATA_FILE = "small_dataset/extra_player_data_100.json"
+FEATURES_PER_PLAYER = 10
+
+# THESE SHOULD BE DICTIONARIES
+
+# Convert lane position to offset into features array
+def get_position_feature_offset(position: str):
+    if position == "TOP":
+        return 0 * FEATURES_PER_PLAYER
+    elif position == "JUNGLE":
+        return 1 * FEATURES_PER_PLAYER
+    elif position == "MIDDLE":
+        return 2 * FEATURES_PER_PLAYER
+    elif position == "BOTTOM":
+        return 3 * FEATURES_PER_PLAYER
+    elif position == "UTILITY":
+        return 4 * FEATURES_PER_PLAYER
+    print("[ERROR] Player has invalid lane position?")
+    return 0
+
+def convert_tier(tier: str):
+    if tier == "IRON":
+        return 0
+    elif tier == "BRONZE":
+        return 1
+    elif tier == "SILVER":
+        return 2
+    elif tier == "GOLD":
+        return 3
+    elif tier == "PLATINUM":
+        return 4
+    elif tier == "DIAMOND":
+        return 5
+    elif tier == "MASTER":
+        return 6
+    elif tier == "GRANDMASTER":
+        return 7
+    elif tier == "CHALLENGER":
+        return 8
+
+    print(f"Player has invalid tier: {tier}")
+    return 0
+
+def convert_rank(rank: str):
+    if rank == "I":
+        return 1
+    elif rank == "II":
+        return 2
+    elif rank == "III":
+        return 3
+    elif rank == "IV":
+        return 4
+
+def get_json_data(filename: str):
+    json_data = {}
+    with open(filename) as f:
+        json_data = json.load(f)
+        f.close()
+    return json_data
+
+def get_features():
+    match_data = get_json_data(MATCH_DATA_FILE)
+    player_data = get_json_data(PLAYER_DATA_FILE)
+    extra_player_data = get_json_data(EXTRA_PLAYER_DATA_FILE)
+
+    features = []
+
+    for match_id, match in match_data.items():
+        # Order of players: B Top, Jungle, Mid, ADC, Supp, R Top, Jungle, Mid, ADC, Supp
+        # Order of features per player: Champion, Tier, Rank, LP, Winrate, Average Kills, Average Deaths, Champion Mastery
+        match_features = [None] * 10 * FEATURES_PER_PLAYER # 10 features per player
+
+        # preprocess the draft so I can access by puuid
+        draft = {}
+        for d in match["draft"]:
+            draft[d["puuid"]] = d
+
+        for index, player in enumerate(match["puuids"]):
+
+            player_draft = draft[player]
+            player_info = player_data[player]
+            extra_player_info = extra_player_data[player_info["encryptedSummonerId"]]
+            
+            position = player_draft["position"]
+            feature_index += get_position_feature_offset(position)
+
+            match_features[(FEATURES_PER_PLAYER * index) + 0] = player_draft["championId"]
+            match_features[(FEATURES_PER_PLAYER * index) + 1] = convert_tier(extra_player_info["tier"])
+            match_features[(FEATURES_PER_PLAYER * index) + 2] = convert_rank(extra_player_info["rank"])
+            match_features[(FEATURES_PER_PLAYER * index) + 3] = extra_player_info["leaguePoints"]
+            match_features[(FEATURES_PER_PLAYER * index) + 4] = extra_player_info["wins"]
+            match_features[(FEATURES_PER_PLAYER * index) + 5] = extra_player_info["losses"]
+            match_features[(FEATURES_PER_PLAYER * index) + 6] = player_info["averageKills"]
+            match_features[(FEATURES_PER_PLAYER * index) + 7] = player_info["averageDeaths"]
+            match_features[(FEATURES_PER_PLAYER * index) + 8] = player_info["averageAssists"]
+            match_features[(FEATURES_PER_PLAYER * index) + 9] = player_info["championMastery"][draft["championId"]]["championPoints"]
+        
+        features.append(match_features)
+        return features
+
+def baseline(features):
+    print("Running baseline test...")
+
+def main():
+    features = get_features()
+    baseline(features)
+
+if __name__ == "__main__":
+    main()
